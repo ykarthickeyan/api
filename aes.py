cc2c0b2f419e36ac5947fef4c6f61a88d6a01c9a
karthickeyan <karthickeyan.yoga@gmail.com>
2021-01-26 00:14:09 +0530

    Update aes.py

--- aes.py
+++ aes.py
@@ -1,18 +1,26 @@
 import requests
 import json
 
-def get_request():
-    return requests.get(url = 'https://fortnite-api.com/v2/aes')
-
 def fetch_dynamic_keys():
-    response = get_request()
+    response = requests.get('https://fortnite-api.com/v2/aes')
     if response.status_code == 200:
         json_data = response.json()
-        write_to_file(json_data['data']['dynamicKeys'])
+        write_to_file(json_data['data']['dynamicKeys'], 'keys.txt')
+    else:
+        print(f'Error: {response.status_code}')
 
-def write_to_file(data):
-    with open('keys.json', 'w') as fw:
+def fetch_items():
+    response = requests.get('https://fortnite-api.com/v2/cosmetics/br/new')
+    if response.status_code == 200:
+        json_data = response.json()
+        write_to_file(json_data['data']['items'], 'new.txt')
+    else:
+        print(f'Error: {response.status_code}')
+
+def write_to_file(data, file_name):
+    with open(file_name, 'w') as fw:
         json.dump(data, fw)
         
 
 fetch_dynamic_keys()
+fetch_items()
