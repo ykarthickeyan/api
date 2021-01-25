4a42660ece25f6432c8a5b92300a9c60778387a3
karthickeyan <karthickeyan.yoga@gmail.com>
2021-01-25 23:56:38 +0530

    Update aes.py

--- aes.py
+++ aes.py
@@ -1,1 +1,18 @@
-https://fortnite-api.com/v2/aes
+import requests
+import json
+
+def get_request():
+    return requests.get(url = 'https://fortnite-api.com/v2/aes')
+
+def fetch_dynamic_keys():
+    response = get_request()
+    if response.status_code == 200:
+        json_data = response.json()
+        write_to_file(json_data['data']['dynamicKeys'])
+
+def write_to_file(data):
+    with open('keys.json', 'w') as fw:
+        json.dump(data, fw)
+        
+
+fetch_dynamic_keys()
